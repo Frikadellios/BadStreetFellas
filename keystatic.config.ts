db1c704d160b878e88cb6ee48b31e12673c8cc98
Daria Ostapenko <145284707+aquaPumpers@users.noreply.github.com>
2023-11-27 22:34:03 +0000

    +

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -161,11 +161,30 @@ export default config({
     services: collection({
         label: 'Services',
         slugField: 'title',
-        path: 'src/content/blog/**',
-        format: { contentField: 'body' },
-        entryLayout: 'content',
+        path: 'src/content/services/**',
+        format: { 
+            data: 'json'
+        },
         schema: {
           title: fields.slug({ name: { label: 'Title' } }),
+          offers: fields.array(
+            fields.relationship({
+              label: 'Offers',
+              description: 'A list of authors for this post',
+              collection: 'offers'
+            }),
+            {
+              label: 'Offers',
+              itemLabel: props => props.value
+            }),
+            description: fields.text({
+                label: 'Description',
+                multiline: true
+              }),
+            heading: fields.text({
+                label: 'Heading Text',
+                multiline: true
+            }),
           featuredMedia: fields.conditional(
               fields.select({
                 label: 'Featured media',
@@ -229,46 +248,106 @@ export default config({
                 itemLabel: props => props.value
               }
             ),
-          publishedDate: fields.date({ label: 'Published date' }),
             videoFile: fields.pathReference({
               label: 'Video file',
               description: 'A reference to a video file in the `public` folder',
               pattern: 'src/assets/**/*',
             }),
-            multi: fields.multiselect({
-              label: 'Category',
-              options: [
-                { label: 'Blog', value: 'blog' },
-                { label: 'Basketball', value: 'basketball' },
-                { label: 'Music', value: 'music' },
-                { label: 'Chess', value: 'chess' },
-              ],
-              defaultValue: ['blog'],
-            }),
-          body: fields.document({
-              label: 'Body',
-              formatting: {
-                  alignment: true,
-                  blockTypes: true,
-                  headingLevels: true,
-                  inlineMarks: {
-                    code: true,
-                    bold: true,
-                    italic: true,
-                    underline: true,
-                    strikethrough: true,
-                  },
-                  listTypes: true,
-                },
-              tables: true,
-              dividers: true,
-              links: true,
-              images: {
-                  directory: 'src/assets/images/services',
-                  publicPath: '../../assets/images/services/',
-              },
+         },
+      }),
+
+    offers: collection({
+        label: 'Offers',
+        slugField: 'title',
+        path: 'src/content/offers/**',
+        format: { 
+            data: 'json'
+        },
+        schema: {
+          title: fields.slug({ name: { label: 'Title' } }),
+          services: fields.relationship({
+            label: 'Service',
+            description: 'This offer relate to this Service Category',
+            collection: 'services'
           }),
+          description: fields.text({
+            label: 'Description',
+            multiline: true
+          }),
+          heading: fields.text({
+            label: 'Heading Text',
+            multiline: true
+          }),
+          featuredMedia: fields.conditional(
+              fields.select({
+                label: 'Featured media',
+                description: 'Optional image/video options for an optional hero media.',
+                options: [
+                  { label: 'No media', value: 'none' },
+                  { label: 'Image', value: 'image' },
+                  { label: 'Video', value: 'video' },
+                ],
+                defaultValue: 'none',
+              }),
+              {
+                none: fields.empty(),
+                image: fields.object({
+                  asset: fields.image({
+                    label: 'Image',
+                    directory: 'src/assets/images/offers',
+                    publicPath: '../../assets/images/offers/',
+                    validation: { isRequired: true },
+                  }),
+                  alt: fields.text({ label: 'Alt', description: 'Image alt text.' }),
+                }),
+                video: fields.object({
+                  url: fields.text({
+                    label: 'A YouTube video URL.',
+                    validation: { length: { min: 1 } },
+                  }),
+                  image: fields.object({
+                    asset: fields.image({
+                      label: 'Image',
+                      description: 'Thumbnail image override for the video.',
+                      directory:  'src/assets/images/offers',
+                      publicPath: '../../assets/images/offers/',
+                    }),
+                    alt: fields.text({ label: 'Alt', description: 'Image alt text.' }),
+                  }),
+                }),
+              }
+            ),
+          seo: fields.conditional(
+              fields.checkbox({ 
+                label: 'Define custom SEO tags', 
+                defaultValue: false,
+              }),
+              {
+                false: fields.empty(),
+                true: fields.object({
+                  title: fields.text({ label: 'Title' }),
+                  description: fields.text({ label: 'Description' }),
+                  url: fields.url({
+                      label: 'URL',
+                      description: 'The website URL'
+                    }),
+                }),
+              }
+            ),
+            tags: fields.array(
+              fields.text({ label: 'Tag' }),
+              {
+                label: 'Tag',
+                itemLabel: props => props.value
+              }
+            ),
+            videoFile: fields.pathReference({
+              label: 'Video file',
+              description: 'A reference to a video file in the `public` folder',
+              pattern: 'src/assets/**/*',
+            }),
          },
       })
-    },
+    
+    }
 });
