c45d40b3cd9c762fd0031327f2966bba2346b010
Daria Ostapenko <145284707+aquaPumpers@users.noreply.github.com>
2023-11-27 01:19:08 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,33 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml';
+import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aquapumpers.com',
+  prefetch: true,
   vite: {
     plugins: [yaml()]
   },
-  integrations: [react(), tailwind({
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'uk', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          uk: 'uk-UA', // The `defaultLocale` value must present in `locales` keys
+          ru: 'ru-UA',
+          en: 'en-US',
+        },
+      },
+    }), 
+    tailwind({
     applyBaseStyles: false
-  })],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
   })
+  ],
+  output: "server",
+  adapter: cloudflare()
 });
